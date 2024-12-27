8cff9473b84862ac393c3cf6c5b2f7d44bbcd478
PARJOAC <pabloarjonilla2005@gmail.com>
2024-12-27 14:11:28 +0100

    Optimización Schemas

---
+++ mongoDB/info.js
@@ -0,0 +1,32 @@
+async function infoSchema(){
+    return {
+        userId: { type: String, required: true, unique: true },
+        balance: { type: Number, default: 20000 },
+        level: { type: Number, default: 1 },
+        experience: { type: Number, default: 0 },
+        maxBet: { type: Number, default: 0 },
+        swag: {
+            balloons: { type: Number, default: 0 },
+            mobile: { type: Number, default: 0 },
+        },
+        lastWork: { type: Number, default: 0 },
+        lastDaily: { type: Number, default: 0 },
+        lastWeekly: { type: Number, default: 0 },
+        lastBlackJack: { type: Number, default: 0 },
+        lastCoinFlip: { type: Number, default: 0 },
+        lastCrash: { type: Number, default: 0 },
+        lastMinesweeper: { type: Number, default: 0 },
+        lastRace: { type: Number, default: 0 },
+        lastRoulette: { type: Number, default: 0 },
+        lastRps: { type: Number, default: 0 },
+        lastRussianRoulette: { type: Number, default: 0 },
+        lastSlot: { type: Number, default: 0 },
+        lastVote: { type: Number, default: 0 },
+        lastCrime: { type: Number, default: 0 },
+        votes: { type: Number, default: 1 },
+    }
+}
+
+module.exports = {
+    infoSchema
+}
