ea7b68df275cd425326548f0a8fa2b0d7af4b360
PARJOAC <pabloarjonilla2005@gmail.com>
2025-01-11 19:10:08 +0100

    serverinfo fix

--- bot/commands/admin/serverinfo.js
+++ bot/commands/admin/serverinfo.js
@@ -1,31 +1,31 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
-const { userCanUseCommand } = require("../../functions/checkAdminCommand");
+const Guild = require("../../../mongoDB/Guild");
 const { greenEmbed } = require("../../functions/interactionEmbed");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("serverinfo")
-    .setDescription("Get information about the server and CasinoBot")
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
-  category: "admin",
-  admin: false,
-  commandId: "1296240894214934529",
-  async execute(interaction, client, lang) {
-    const check = await userCanUseCommand(interaction, lang, client);
-    if (check.status) return;
+    data: new SlashCommandBuilder()
+        .setName("serverinfo")
+        .setDescription("Get information about the server and CasinoBot")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    category: "admin",
+    admin: false,
+    commandId: "1296240894214934529",
+    async execute(interaction, client, lang) {
+        const guildData = await Guild.findOne({ guildId: interaction.guild.id });
 
-    return greenEmbed(interaction, client, {
-       title: lang.serverInfoTitle,
-       fields: [
-        { name: lang.serverName, value: interaction.guild.name, inline: true },
-        { name: lang.serverOwner, value: interaction.guild.owner.user.tag, inline: true },
-        { name: lang.serverMembers, value: interaction.guild.memberCount, inline: true },
-        { name: lang.economyType, value: check.economyType ? lang.activated : lang.deactivated, inline: true }
-       ],
-       thumbnail: interaction.guild.iconURL({ dynamic: true }),
-       footer: client.user.username,
-       ephemeral: false
-    });
+        return greenEmbed(interaction, client, {
+            type: "editReply",
+            title: lang.serverInfoTitle,
+            fields: [
+                { name: lang.serverName, value: interaction.guild.name, inline: true },
+                { name: lang.serverOwner, value: interaction.guild.owner.user.tag, inline: true },
+                { name: lang.serverMembers, value: interaction.guild.memberCount, inline: true },
+                { name: lang.economyType, value: guildData.economyType ? lang.activated : lang.deactivated, inline: true }
+            ],
+            thumbnail: interaction.guild.iconURL({ dynamic: true }),
+            footer: client.user.username,
+            ephemeral: false
+        });
 
-  },
+    },
 };
