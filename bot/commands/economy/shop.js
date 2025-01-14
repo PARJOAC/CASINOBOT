8e37c9f9ec7d3f36f4fc50a84b024779520c14e1
PARJOAC <pabloarjonilla2005@gmail.com>
2025-01-14 18:27:11 +0100

    Casi finalizado las compras

--- bot/commands/economy/shop.js
+++ bot/commands/economy/shop.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require("discord.js");
 const { redEmbed, greenEmbed } = require("../../functions/interactionEmbed");
 const { addSet, delSet, getSet } = require("../../functions/getSet");
 const paypal = require("@paypal/checkout-server-sdk");
+const { userCanUseCommand } = require("../../functions/checkAdminCommand");
 
 const PlayerBoost = require("../../../mongoDB/PlayerBoost");
 
@@ -10,44 +11,6 @@ const environment = new paypal.core.SandboxEnvironment(
     process.env.PAYPAL_CLIENT_SECRET
 );
 const paypalClient = new paypal.core.PayPalHttpClient(environment);
-
-// Precios base
-const basePrices = {
-    "boostx2_30min": { price: 3.99, description: "Multiplica x2 tus ganancias por 30 minutos." },
-    "boostx5_1h": { price: 3.99, description: "Multiplica x5 tus ganancias por 1 hora." },
-    "coins_30k": { price: 4.99, description: "Obtén 30,000 monedas instantáneamente." },
-    "coins_100k": { price: 12.99, description: "Obtén 100,000 monedas instantáneamente." },
-    "coins_500k": { price: 59.99, description: "Obtén 500,000 monedas instantáneamente." },
-    "vip_30d": { price: 9.99, description: "Ventajas VIP por 30 días." },
-    "xpx2_1h": { price: 2.49, description: "Multiplica x2 tu experiencia por 1 hora." },
-    "xpx5_3h": { price: 4.99, description: "Multiplica x5 tu experiencia por 3 horas." },
-    "xpx10_24h": { price: 8.99, description: "Multiplica x10 tu experiencia por 24 horas." },
-    "lvlup10": { price: 3.99, description: "Sube 10 niveles instantáneamente." },
-    "lvlup20": { price: 6.99, description: "Sube 20 niveles instantáneamente." },
-    "lvlup50": { price: 8.99, description: "Sube 50 niveles instantáneamente." },
-};
-
-// Función para calcular precios ajustados
-async function calculatePricesWithVip(userId) {
-    const player = await PlayerBoost.findOne({ userId });
-
-    // Determinar si el jugador es VIP
-    const isVip = player && player.isVipActive();
-
-    // Aplicar descuento del 20% si es VIP
-    const discountMultiplier = isVip ? 0.8 : 1;
-
-    // Calcular precios ajustados
-    const adjustedPrices = {};
-    for (const [key, { price, description }] of Object.entries(basePrices)) {
-        adjustedPrices[key] = {
-            price: (price * discountMultiplier).toFixed(2), // Asegurarse de que sea número
-            description, // Propagar descripción
-        };
-    }
-
-    return adjustedPrices;
-}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -59,16 +22,16 @@ module.exports = {
                 .setDescription("Select the item you want to buy")
                 .setRequired(false)
                 .addChoices(
-                    { name: "Boost x2 (30 minutes)", value: "boostx2_30min" },
-                    { name: "Boost x5 (1 hour)", value: "boostx5_1h" },
-                    { name: "Boost x10 (2 hours)", value: "boostx10_2h" },
+                    { name: "Multiplier +0.25 (permanent)", value: "multiplier_x0.25" },
+                    { name: "Multiplier +0.5 (permanent)", value: "multiplier_x0.5" },
+                    { name: "Multiplier +0.75 (permanent)", value: "multiplier_x0.75" },
+                    { name: "Coins 30k (instant)", value: "coins_30k" },
+                    { name: "Coins 100k (instant)", value: "coins_100k" },
+                    { name: "Coins 500k (instant)", value: "coins_500k" },
+                    { name: "Level Up +10 (instant)", value: "level_+10" },
+                    { name: "Level Up +20 (instant)", value: "level_+20" },
+                    { name: "Level Up +50 (instant)", value: "level_+50" },
                     { name: "Player VIP (30 days)", value: "vip_30d" },
-                    { name: "XP x2 (1 hour)", value: "xpx2_1h" },
-                    { name: "XP x5 (3 hours)", value: "xpx5_3h" },
-                    { name: "XP x10 (24 hours)", value: "xpx10_24h" },
-                    { name: "Level Up +10 (instant)", value: "lvlup10" },
-                    { name: "Level Up +20 (instant)", value: "lvlup20" },
-                    { name: "Level Up +50 (instant)", value: "lvlup50" },
                 )
         )
         .addIntegerOption((option) =>
@@ -81,8 +44,48 @@ module.exports = {
     commandId: "1296240894214934532",
 
     async execute(interaction, client, lang, playerData) {
+        // Check if the user can use this command
+        const check = await userCanUseCommand(interaction, lang, client);
+        if (check.status) return;
+
         const executing = await getSet(interaction, lang, client);
         if (executing) return;
+
+        // Precios base
+        const basePrices = {
+            "multiplier_x0.25": { price: 5.99, description: lang.buyMultiplier.replace("{multiplier}", 0.25) },
+            "multiplier_x0.5": { price: 8.99, description: lang.buyMultiplier.replace("{multiplier}", 0.5) },
+            "multiplier_x0.75": { price: 10.99, description: lang.buyMultiplier.replace("{multiplier}", 0.75) },
+            "coins_30k": { price: 3.99, description: lang.buyCoins.replace("{coins}", Number(30000).toLocaleString()) },
+            "coins_100k": { price: 11.99, description: lang.buyCoins.replace("{coins}", Number(100000).toLocaleString()) },
+            "coins_500k": { price: 39.99, description: lang.buyCoins.replace("{coins}", Number(500000).toLocaleString()) },
+            "level_+10": { price: 3.99, description: lang.buyLevel.replace("{level}", 10) },
+            "level_+20": { price: 6.49, description: lang.buyLevel.replace("{level}", 20) },
+            "level_+50": { price: 9.99, description: lang.buyLevel.replace("{level}", 50) },
+            "vip_30d": { price: 4.99, description: lang.buyVip },
+        };
+
+        // Función para calcular precios ajustados
+        async function calculatePricesWithVip(userId) {
+            const player = await PlayerBoost.findOne({ userId });
+
+            // Determinar si el jugador es VIP
+            const isVip = player && player.isVipActive();
+
+            // Aplicar descuento del 20% si es VIP
+            const discountMultiplier = isVip ? 0.8 : 1;
+
+            // Calcular precios ajustados
+            const adjustedPrices = {};
+            for (const [key, { price, description }] of Object.entries(basePrices)) {
+                adjustedPrices[key] = {
+                    price: (price * discountMultiplier).toFixed(2), // Asegurarse de que sea número
+                    description, // Propagar descripción
+                };
+            }
+
+            return adjustedPrices;
+        }
 
         await addSet(interaction.user.id);
 
@@ -92,28 +95,30 @@ module.exports = {
 
         // Si no se selecciona un ítem, mostrar la lista de precios y recompensas
         if (!item) {
-            let priceList = "**Lista de Precios y Recompensas:**\n";
+            await delSet(interaction.user.id);
+            let descriptionList;
             for (const [key, { price, description }] of Object.entries(prices)) {
-                priceList += `• **${key.replace(/_/g, ' ').replace(/\b\w/g, char => char.toUpperCase())}**: €${price} - ${description}\n`;
+                descriptionList += `• **${key.replace(/_/g, ' ').replace(/\b\w/g, char => char.toUpperCase())}**: ${price}€ - ${description}\n\n`;
             }
 
             return greenEmbed(interaction, client, {
                 type: "editReply",
-                title: "Tienda de CasinoBot",
-                description: priceList,
+                title: lang.buyItemTitle,
+                description: descriptionList,
                 footer: client.user.username,
                 ephemeral: true,
             });
         }
-        
+
         const quantity = interaction.options.getInteger("quantity") || 1;
 
         if (quantity <= 0) {
             await delSet(interaction.user.id);
             return redEmbed(interaction, client, {
                 type: "editReply",
-                title: "Error",
-                description: "Quantity must be greater than 0.",
+                title: lang.errorTitle,
+                description: lang.negativeItem,
+                thumbnail: client.user.displayAvatarURL(),
                 footer: client.user.username,
                 ephemeral: true,
             });
@@ -125,13 +130,13 @@ module.exports = {
             await delSet(interaction.user.id);
             return redEmbed(interaction, client, {
                 type: "editReply",
-                title: "Error",
-                description: "Invalid item selected.",
+                title: lang.errorTitle,
+                description: lang.invalidItemSelect,
                 footer: client.user.username,
                 ephemeral: true,
             });
         }
-        const totalPrice = (price * quantity).toFixed(2);
+        const totalPrice = (parseFloat(price.price) * quantity).toFixed(2);
 
         // Crear la orden de PayPal
         const orderRequest = new paypal.orders.OrdersCreateRequest();
@@ -160,8 +165,9 @@ module.exports = {
             // Mensaje inicial de compra pendiente
             await greenEmbed(interaction, client, {
                 type: "editReply",
-                title: "Compra Pendiente",
-                description: `Haz clic [aquí](${approvalLink}) para completar tu pago de €${totalPrice}. Tienes 15 minutos para completar la compra.`,
+                title: lang.purchaseInit,
+                description: lang.purchaseInitContent.replace("{category}", item.replace(/_/g, ' ').replace(/\b\w/g, char => char.toUpperCase())).replace("{url}", approvalLink).replace("{price}", totalPrice),
+                thumbnail: client.user.displayAvatarURL(),
                 footer: client.user.username,
                 ephemeral: true,
             });
@@ -172,17 +178,11 @@ module.exports = {
 
                     if (orderDetails.result.status !== "APPROVED") {
                         await delSet(interaction.user.id);
-                        await redEmbed(interaction, client, {
+                        return redEmbed(interaction, client, {
                             type: "editReply",
-                            title: "Tiempo Agotado",
-                            description: "La orden ha expirado después de 15 minutos y ha sido cancelada.",
+                            title: lang.timeException,
+                            description: lang.buyTimeExceptionContent,
                             footer: client.user.username,
-                            ephemeral: true,
-                        });
-
-                        // Notificar al usuario de la cancelación de la compra
-                        await interaction.followUp({
-                            content: `La compra de ${quantity} x ${item.replace(/_/g, ' ').replace(/\b\w/g, char => char.toUpperCase())} por €${totalPrice} ha sido cancelada debido a la expiración del tiempo.`,
                             ephemeral: true,
                         });
                     }
@@ -213,44 +213,36 @@ module.exports = {
                             await boostData.save();
                         };
 
-
                         // Guardar la compra de Boost o Multiplicador en el esquema
-                        if (item.includes("boost")) {
-                            const boostExpiration = new Date();
-                            if (item === "boostx2_30min") boostExpiration.setMinutes(boostExpiration.getMinutes() + (quantity * 30));
-                            else if (item === "boostx5_1h") boostExpiration.setHours(boostExpiration.getHours() + (quantity * 1));
-                            // Se pueden agregar más tipos de Boost si es necesario
-
-                            boostData.boosts.set(item, boostExpiration);
-                        }
-
-                        if (item.includes("xpx")) {
-                            const xpMultiplierExpiration = new Date();
-                            if (item === "xpx2_1h") xpMultiplierExpiration.setHours(xpMultiplierExpiration.getHours() + (quantity * 1));
-                            else if (item === "xpx5_3h") xpMultiplierExpiration.setHours(xpMultiplierExpiration.getHours() + (quantity * 3));
-                            // Se pueden agregar más tipos de XP Boost si es necesario
-
-                            boostData.boosts.set(item, xpMultiplierExpiration);
-                        }
-                        
                         if (item.includes("coins")) {
-                            if (item === "coins_30k") playerData.balance += 30000;
-                            else if (item === "coins_100k") playerData.balance += 100000;
-                            else if (item === "coins_500k") playerData.balance += 500000;
+                            if (item === "coins_30k") playerData.balance += 30000 * quantity;
+                            else if (item === "coins_100k") playerData.balance += 100000 * quantity;
+                            else if (item === "coins_500k") playerData.balance += 500000 * quantity;
                             await playerData.save();
                         }
-                        
-                        if (item.includes("levelup")) {
-                            if (item === "levelup10") playerData.level += 10;
-                            else if (item === "levelup20") playerData.level += 20;
-                            else if (item === "levelup50") playerData.level += 50;
+
+                        if (item.includes("level")) {
+                            if (item === "level_+10") playerData.level += 10 * quantity;
+                            else if (item === "level_+20") playerData.level += 20 * quantity;
+                            else if (item === "level_+50") playerData.level += 50 * quantity;
+                            await playerData.save();
+                        }
+
+                        if (item.includes("multiplier")) {
+                            if (item === "multiplier_x0.25") playerData.multiplier += 0.25 * quantity;
+                            else if (item === "multiplier_x0.5") playerData.multiplier += 0.5 * quantity;
+                            else if (item === "multiplier_x0.75") playerData.multiplier += 0.75 * quantity;
                             await playerData.save();
                         }
 
                         // Guardar la compra del VIP
                         if (item === "vip_30d") {
-                            const vipExpiration = new Date();
-                            vipExpiration.setDate(vipExpiration.getDate() + (quantity * 30)); // 30 días de expiración
+                            const currentDate = new Date();
+                            const vipExpiration = boostData.vipExpiration && boostData.vipExpiration > currentDate
+                                ? new Date(boostData.vipExpiration)
+                                : currentDate;
+
+                            vipExpiration.setDate(vipExpiration.getDate() + (quantity * 30)); // Agregar 30 días por cada compra
                             boostData.isVip = true;
                             boostData.vipExpiration = vipExpiration;
                         }
@@ -258,17 +250,11 @@ module.exports = {
                         // Guardar los cambios
                         await boostData.save();
 
-                        await greenEmbed(interaction, client, {
+                        return greenEmbed(interaction, client, {
                             type: "editReply",
-                            title: "Compra Completada",
-                            description: `¡Tu compra de ${quantity} x ${item.replace(/_/g, ' ').replace(/\b\w/g, char => char.toUpperCase())} por €${totalPrice} ha sido completada con éxito!`,
+                            title: lang.purchaseSuccess,
+                            description: lang.purchaseSuccessContent.replace("{quantity}", quantity).replace("{item}", item.replace(/_/g, ' ').replace(/\b\w/g, char => char.toUpperCase())).replace("{price}", totalPrice),
                             footer: client.user.username,
-                            ephemeral: true,
-                        });
-
-                        // Notificar al usuario del éxito de la compra
-                        await interaction.followUp({
-                            content: `Compra exitosa de ${quantity} x ${item.replace(/_/g, ' ').replace(/\b\w/g, char => char.toUpperCase())} por €${totalPrice}.`,
                             ephemeral: true,
                         });
                     }
@@ -282,8 +268,8 @@ module.exports = {
             await delSet(interaction.user.id);
             return redEmbed(interaction, client, {
                 type: "editReply",
-                title: "Error",
-                description: "Hubo un problema al crear tu orden de PayPal. Por favor, intenta de nuevo más tarde.",
+                title: lang.errorTitle,
+                description: lang.purchaseError,
                 footer: client.user.username,
                 ephemeral: true,
             });
